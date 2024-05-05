16391b1339f4cecbad1760b38339322001120ee8
Elvira Lebedeva <allay1984@yandex.ru>
2024-05-05 18:39:12 +0300

    test commit

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,2 +1,18 @@
-package org.example;public class Client {
+package org.example;
+
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class Client {
+    protected static final String BASE_URI = "http://qa-scooter.praktikum-services.ru";
+    protected static final String BASE_PATH = "/api/v1";
+
+    public RequestSpecification spec() {
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .baseUri(BASE_URI)
+                .basePath(BASE_PATH);
+    }
 }
--- src/test/java/ListOfOrdersTests.java
+++ src/test/java/ListOfOrdersTests.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ListOfOrdersTests {
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.example.orders.AssertOrder;
+import org.example.orders.OrderClient;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListOfOrdersTests {
+    private OrderClient orderClient;
+    private AssertOrder assertOrderVoid = new AssertOrder();
+
+    @Before
+    public void setUp() {
+        orderClient = new OrderClient();
+    }
+
+    @Test
+    @DisplayName("Проверка получения в теле ответа списка заказов")
+    @Description("List of orders test. GET/api/v1/orders")
+    public void ListOfOrders() {
+        ValidatableResponse response = orderClient.getOrderList();
+        assertOrderVoid.getOrderListSuccessfully(response);
+    }
 }
